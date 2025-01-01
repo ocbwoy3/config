3dc12673d62c9c966193a5a41d7d45a6c4b2c2d6
OCbwoy3 <ocbwoy3@ocbwoy3.dev>
2025-01-01 16:26:51 +0200

    ah bru

--- hosts/default/other/distro.nix
+++ hosts/default/other/distro.nix
@@ -1,22 +1,12 @@
-{ config, inputs, pkgs, ... }:
+{ config, inputs, pkgs, lib, ... }:
 
 {
 
-	environment.etc."os-release" = {
-		source = pkgs.writeText "os-release" ''
-NAME="Arch Linux"
-PRETTY_NAME="Arch Linux"
-ID=arch
-BUILD_ID=rolling
-ANSI_COLOR="38;2;23;147;209"
-HOME_URL="https://archlinux.org/"
-DOCUMENTATION_URL="https://wiki.archlinux.org/"
-SUPPORT_URL="https://bbs.archlinux.org/"
-BUG_REPORT_URL="https://gitlab.archlinux.org/groups/archlinux/-/issues"
-PRIVACY_POLICY_URL="https://terms.archlinux.org/docs/privacy-policy/"
-LOGO=archlinux-logo
+	system.activationScripts.installRoblox = {
+		text = ''
+			flatpak install https://sober.vinegarhq.org/sober.flatpakref
 		'';
-		mode = "0644";
 	};
+
 
 }
