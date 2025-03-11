103e736cf90858b2f20f441077f6739178a64af2
OCbwoy3 <ocbwoy3@ocbwoy3.dev>
2025-03-11 22:48:04 +0200

    why

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 lsblk
 
-echo "Enter disk to install to: (like /dev/sda but only enter the sda part)"
-read vardisk
+printf "Installing OCbwoy3's Dotfiles\nSit back and wait until NixOS (and the config) gets installed, or do something in the meantime.\n\n"
 
-sudo nix run 'github:nix-community/disko/latest#disko-install' --extra-experimental-features "nix-command flakes" -- --flake .#default --disk main /dev/$vardisk
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount hosts/default/disko.nix
+sudo nixos-install --flake .#default
+
+# sudo nix run 'github:nix-community/disko/latest#disko-install' --extra-experimental-features "nix-command flakes" -- --flake .#default --disk main /dev/$vardisk
