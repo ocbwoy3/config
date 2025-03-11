12ad0b4107f92e6d43d61effe4e90f55233655f4
OCbwoy3 <ocbwoy3@ocbwoy3.dev>
2025-03-11 22:41:29 +0200

    main

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ lsblk
 echo "Enter disk to install to: (like /dev/sda but only enter the sda part)"
 read vardisk
 
-sudo nix run 'github:nix-community/disko/latest#disko-install' --extra-experimental-features "nix-command flakes" -- --flake .#default --disk $vardisk /dev/$vardisk
+sudo nix run 'github:nix-community/disko/latest#disko-install' --extra-experimental-features "nix-command flakes" -- --flake .#default --disk main /dev/$vardisk
