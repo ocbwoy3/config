11ba8c2cf98538bcf821d15ceb74fd136a483274
OCbwoy3 <ocbwoy3@ocbwoy3.dev>
2025-03-11 23:06:06 +0200

    testtttttt

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 lsblk
 
-printf "Installing OCbwoy3's Dotfiles\nSit back and wait until NixOS (and the config) gets installed, or do something in the meantime.\n\n"
+printf "\n\nWelcome to OCbwoy3's Dotfiles!!\n\nThis will be semi-automatic until the install phase. Now, let's wipe all your disks and partition them.\n\n"
 
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount hosts/default/disko.nix
+
+printf "\n\nInstalling NixOS, plz something in the meantime.\n\n"
+
 sudo nixos-install --flake .#default
 
 # sudo nix run 'github:nix-community/disko/latest#disko-install' --extra-experimental-features "nix-command flakes" -- --flake .#default --disk main /dev/$vardisk
