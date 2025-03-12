f679d767a3f58243998b1a3edebe34f401132b50
OCbwoy3 <ocbwoy3@ocbwoy3.dev>
2025-03-12 21:36:50 +0200

    screenshot edit

---
+++ scripts/edit-last-screenshot.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCREENSHOTS_DIR="$HOME/Pictures/Screenshots"
+
+LAST_SCREENSHOT=$(ls -t "$SCREENSHOTS_DIR" | head -n 1)
+
+if [[ -n "$LAST_SCREENSHOT" ]]; then
+    swappy -f "$SCREENSHOTS_DIR/$LAST_SCREENSHOT" -o "$SCREENSHOTS_DIR/$LAST_SCREENSHOT"
+else
+    notify-send -u critical -t 3 "Error" "Can't find screenshot :("
+fi
--- scripts/screenshot-fullscreen.sh
+++ scripts/screenshot-fullscreen.sh
@@ -7,5 +7,5 @@ echo "$SS_PATH"
 
 if [[ -n "$SS_PATH" && "$SS_PATH" == /home/ocbwoy3/Pictures/Screenshots/* ]]; then
     wl-copy < "$SS_PATH"
-    notify-send "Screenshot" "Successfully captured screenshot to clipboard"
+    notify-send "Screenshot" "Successfully captured screenshot to clipboard.\n<small>Open swappy with ALT+SHIFT+PS.</small>"
 fi
