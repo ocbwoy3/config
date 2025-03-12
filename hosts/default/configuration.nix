2976cc35f92704fde640796d19b55d92b7356758
OCbwoy3 <ocbwoy3@ocbwoy3.dev>
2025-03-12 00:01:21 +0000

    killed swap

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -155,10 +155,7 @@
 		"/".options = [ "compress=zstd" ];
 		"/home".options = [ "compress=zstd" ];
 		"/nix".options = [ "compress=zstd" "noatime" ];
-		"/swap".options = [ "noatime" ];
 	};
-
-	swapDevices = [ { device = "/swap/swapfile"; } ];
 
 	services.btrfs.autoScrub = {
 		enable = true;
