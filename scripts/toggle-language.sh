1032846591c190db2d98d72702d8ea99095ccfcb
OCbwoy3 <ocbwoy3@ocbwoy3.dev>
2025-06-23 16:18:36 +0300

    fix input again!!!!

--- scripts/toggle-language.sh
+++ scripts/toggle-language.sh
@@ -1,16 +1,6 @@
 KBLANG=$(hyprctl getoption input:kb_layout | awk 'NR==1{print $2}')
-if [ "$KBLANG" = "us" ] ; then
-	hyprctl --batch "\
-		keyword input:kb_layout lv;\
-		keyword input:kb_variant apostrophe;"
-		# keyword input:kb_options compose:apostrophe;"
-	notify-send -u low "Tastatūra" "🇱🇻 Valoda: Latviešu"
-	exit
-elif [ "$KBLANG" = "lv" ] ; then
-	hyprctl --batch "\
-		keyword input:kb_layout us;\
-		keyword input:kb_variant \"\";"
-		# keyword input:kb_options compose:apostrophe;"
-	notify-send -u low "Tastatūra" "🇺🇸 Valoda: Angļu"
-	exit
-fi
+
+# hyprctl switchxkblayout semico--usb-gaming-keyboard- next
+
+hyprctl switchxkblayout current next
+notify-send -u low "Tastatūra" "Valoda mainīta!"
