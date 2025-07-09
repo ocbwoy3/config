143a17c686f3987dd63dcd6399efb0ce03035a58
OCbwoy3 <ocbwoy3@ocbwoy3.dev>
2025-07-09 13:27:41 +0300

    remove latvian language from roblox chooser

--- scripts/open-roblox-game.sh
+++ scripts/open-roblox-game.sh
@@ -2,7 +2,7 @@
 
 GAMES=$(cat ~/config/scripts/lib/games.txt)
 
-SELECTED=$(echo "$GAMES" | cut -d'%' -f1 | wofi --show dmenu -p "Uzspēlē Roblox...")
+SELECTED=$(echo "$GAMES" | cut -d'%' -f1 | wofi --show dmenu -p "Play Roblox...")
 
 if [ -n "$SELECTED" ]; then
 	PLACE_ID=$(echo "$GAMES" | grep "^$SELECTED" | sed 's/.*%%% //')
