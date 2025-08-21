fd1090d9edb665c851794e9baf5177a46b5119eb
OCbwoy3 <ocbwoy3@ocbwoy3.dev>
2025-08-21 14:44:22 +0300

    fix

--- hosts/server/modules/knot.nix
+++ hosts/server/modules/knot.nix
@@ -4,7 +4,6 @@
 	services.tangled-knot = {
 		enable = true;
 		listenAddr = "0.0.0.0:3003";
-		dataDir = "/var/lib/knot";
 		secretFile = "/private/tangled.env";
 		hostname = "knot.ocbwoy3.dev";
 	};
